f5dc8dc1bf619d60347781b397aaaacddfe5d827
Armin <dzarma.busatlic@gmail.com>
2025-09-01 11:59:47 +0200

    initial commit, integrating Kinde for authentication

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-20">
+        Dashboard <span className="text-blue-500">Page</span>
+      </h1>
+      <p className="text-center mt-4">This is the dashboard page.</p>
+    </div>
+  );
+}
